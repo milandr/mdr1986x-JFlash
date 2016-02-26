e01db178a4e5631b7f453f48ddc1ee4a89ea9625
Vitaly Kravtsov <in4lio@gmail.com>
2016-02-26 14:13:28 +0300

    improved batch

--- JFlash.bat
+++ JFlash.bat
@@ -1,1 +1,4 @@
-@arm-none-eabi-gdb-py --batch -x JFlash.py -ex "py program_from_shell('%1')"
+@echo off
+start /B JLinkGDBServerCL.exe -if swd -device "Cortex-M3" -endian little -speed 2000 -port 2331 -vd -localhostonly 1 -singlerun -strict -notimeout
+arm-none-eabi-gdb-py --batch -x JFlash.py -ex "py program_from_shell('%1')"
+timeout 1 > NUL
